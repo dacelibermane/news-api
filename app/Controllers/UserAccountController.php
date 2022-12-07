976583646668de1b0ff9382717577c0097d504d7
dacelibermane <dace.libermane@gmail.com>
2022-12-07 10:58:47 +0200

    update user controller

--- app/Controllers/UserAccountController.php
+++ app/Controllers/UserAccountController.php
@@ -22,5 +22,18 @@ class UserAccountController
             ->where('email = ?')
             ->setParameter(0, $_SESSION['auth_id'])
             ->fetchAssociative();
+
+        $validPassword = password_verify($_POST['password_old'], $user['password']) ;
+        if(!$validPassword){
+            $_SESSION['errors']['old_password_match'] = "Invalid old password";
+        }
+        if ($_POST['password_new'] !== $_POST['password_confirm']) {
+            $_SESSION['errors']['password_repeat'] = "Passwords do not match";
+        }
+
+        if (count($_SESSION['errors']) > 0) {
+            return new Redirect('/login');
+        }
+        return new Redirect('/account');
     }
 }
